480923cea88421a93cd64146e8e2550577d4b91c
yeram <ramye@naver.com>
2023-09-20 14:48:45 +0900

    😺[CHORE] list view

---
+++ likelion/src/App.js
@@ -0,0 +1,125 @@
+function App() {
+  return (
+    <>
+      <article>
+        {/* 아이템 리스트 */}
+        <section className={"list"}>
+          <h1>아이템 리스트</h1>
+          <ol>
+            {/* 아이템 A */}
+            <li>
+              <span>A</span>
+              <div>
+                <div>0</div>
+                <div className={"button-list"}>
+                  <button>-</button>
+                  <button>+</button>
+                </div>
+              </div>
+            </li>
+            {/* 아이템 B */}
+            <li>
+              <span>B</span>
+              <div>
+                <div>0</div>
+                <div className={"button-list"}>
+                  <button>-</button>
+                  <button>+</button>
+                </div>
+              </div>
+            </li>
+            {/* 아이템 C */}
+            <li>
+              <span>C</span>
+              <div>
+                <div>0</div>
+                <div className={"button-list"}>
+                  <button>-</button>
+                  <button>+</button>
+                </div>
+              </div>
+            </li>
+          </ol>
+        </section>
+      </article>
+      <style jsx>{`
+        article {
+          height: 100vh;
+          width: 100vw;
+          max-width: 30rem;
+          margin: 0 auto;
+          padding: 1.5rem;
+          background: #ffc0cb21;
+        }
+
+        article h1 {
+          padding: 0 0.5rem;
+        }
+        section.list {
+          display: flex;
+          flex-direction: column;
+        }
+        section.list ol {
+          list-style-type: none;
+          padding: 0;
+          margin: 0;
+          display: flex;
+          flex-direction: column;
+          gap: 0.5rem;
+        }
+        li {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          padding: 1rem;
+          border-radius: 0.5rem;
+          background-color: white;
+          box-shadow: 0px 2px 10px 0px rgb(182 158 198 / 15%);
+          height: 2rem;
+        }
+        li > span {
+          font-size: 1.5rem;
+          font-weight: 500;
+        }
+        li > div {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          gap: 1rem;
+        }
+        li > div > div:first-child {
+          display: flex;
+          justify-content: flex-end;
+          align-items: center;
+          padding: 0 0.5rem;
+          width: 1.5rem;
+          height: 1.5rem;
+          border-radius: 5px;
+          background: #efeded;
+          font-weight: 700;
+        }
+        li > div > div.button-list {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          gap: 0.5rem;
+        }
+        li > div > div.button-list button {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          width: 1.7rem;
+          height: 1.7rem;
+          cursor: pointer;
+          border-radius: 5px;
+          border: none;
+          background: #f6ecff;
+          font-size: 1rem;
+          font-weight: 500;
+        }
+      `}</style>
+    </>
+  );
+}
+
+export default App;
